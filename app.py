96b3d0e580e71120f538d61a8e43673780b78592
Gent23 <133636576+Gent23@users.noreply.github.com>
2023-05-15 13:44:12 +0000

    Added Jinja templates

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [
+  {
+    "id": 1,
+    "name": "Python Developer",
+    "location": "India"
+  },
+  {
+    "id": 2,
+    "name": "C++ Developer",
+    "location": "India"
+  },
+  {
+    "id": 3,
+    "name": "Java Developer",
+    "location": "India"
+  },
+]
+
 
 @app.route("/")
 def home():
-  return render_template("home.html")
+  return render_template("home.html", jobs=jobs)
 
 
 if __name__ == "__main__":
